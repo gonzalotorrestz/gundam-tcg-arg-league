38ea693dfd7916a8ccc3d7bc00a9134375cea3a1
Gonzalo Torres Tonkowicz <gtorres@nacionservicios.com.ar>
2025-12-23 11:05:02 -0300

    Archivos para Pages

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJBBQbo3Wzd-mSwtoSBNN4d7fw9tDn4Po",
+  authDomain: "gundam-tcg-league.firebaseapp.com",
+  projectId: "gundam-tcg-league",
+  storageBucket: "gundam-tcg-league.firebasestorage.app",
+  messagingSenderId: "1085104329166",
+  appId: "1:1085104329166:web:ca3e932ab0e27874409b9f"
+};
+
+// Exportar la configuración
+export { firebaseConfig };
